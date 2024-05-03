770046d31c50c6fd14b7f9938534532a64822bbd
Ntunyu Serge <ntunyuserge@gmail.com>
2024-05-03 08:52:31 +0100

    Initialize project

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:first_app/gradient_container.dart';
 
-
-void main() { //entry point
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(
-          Color.fromARGB(255, 26, 2, 80),
-          Color.fromARGB(255, 45, 7, 80),
-        ),
-      ),
-    ),
-  );
-}
